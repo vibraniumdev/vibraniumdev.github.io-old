5e7dab95de77924908c8c392dd3dd35cb45d52f8
guiemi <guiemi.teixeira@gmail.com>
2020-05-30 16:19:33 -0300

    Adiciona o plugin da API ao Jekyll

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@
 
 source "https://rubygems.org"
 gemspec
+
+gem "jekyll-get-json", "~> 0.0.2"
--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
-(function(){
+// Animação
+(function () {
   'use strict';
 
   new WOW().init();
-  
+
 })();
+
+// API GitHub
+
+// (function () {
+//   const user = "guiemi";
+//   const url = "https://api.github.com/users/guiemi/repos";
+//   const client_id = "9d71a8c4106f4ea74ef9";
+//   const client_secret = "f9ff9a14fad8c26a809624c6bd2007690311b4af";
+
+//   async function getUser(user) {
+//     const profile = await fetch(`${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+//     const profile_json = profile.json();
+
+//     return profile_json
+
+//     console.log(user)
+//   }
+
+//   getUser(user).then(res => console.log(res))
+
+// })();
