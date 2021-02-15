45dcbfed2479ee6dc93a782959b44156ff2db2a5
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2021-02-15 20:37:43 -0300

    After deploy

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/guiemi.github.io/src/pages/404.js"))),
-  "component---src-pages-blog-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/guiemi.github.io/src/pages/blog.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/guiemi.github.io/src/pages/index.js"))),
-  "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/guiemi.github.io/src/templates/blog-post.js")))
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/vibranium.dev/src/pages/404.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/vibranium.dev/src/pages/blog.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/vibranium.dev/src/pages/index.js"))),
+  "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/vibranium.dev/src/templates/blog-post.js")))
 }
 
