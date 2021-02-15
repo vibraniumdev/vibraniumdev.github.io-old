d1ccd15a291603d2f8d64881186aa4b5db825401
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2021-02-15 20:00:33 -0300

    Files chances after deploy

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/guiemi.github.io/src/pages/404.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/guiemi.github.io/src/pages/blog.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/guiemi.github.io/src/pages/index.js"))),
+  "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/guiemi/GitHub/websites/guiemi.github.io/src/templates/blog-post.js")))
+}
+
