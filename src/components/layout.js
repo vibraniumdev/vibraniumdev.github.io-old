9b452fb2a5cd90710c2d7742f38ac35d7aa3fce0
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2021-02-17 17:32:32 -0300

    Changes the blue heart to the black one

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,7 @@ const Layout = ({ children }) => (
           <main>{children}</main>
           <S.Footer>
             <p>
-              © {new Date().getFullYear()}, Made with &#128153;  and
+              © {new Date().getFullYear()}, Made with &#x1F5A4; and
             </p>
             <S.GatsbyLink href="https://www.gatsbyjs.org">Gatsby</S.GatsbyLink>
           </S.Footer>
