487b59b740d399b06a2bfb20b448377be8ae5c58
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2021-02-17 00:11:18 -0300

    Updates the About component and sets its styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,16 +2,16 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import * as S from '../components/layout.styles'
+import * as S from './about.styles'
 
 const About = () => (
   <Layout>
     <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
     <S.Content>
-      <h1>Eu sou uma página de about</h1>
-      <p>Hello, my name is Guilherme. I'm a Computational Linguistics intern, a developer, and a linguistics undergraduate student based in São Carlos, Brazil.</p>
-      <p>I love computers and inline-skating since the childhood; and I love to read about linguistics and science in general. I love to read science fiction, and fantasy literature too. I also enjoy watching NBA on TV in the spare time.</p>
-      <p>I'm currently improving my Python and NLP skills in my actual internship as a computational linguist. I'm also studying JavaScript and Vue.js. My main goal in the college is to develop research on Generative Grammar (a.k.a. Chomskyan Syntax) and Formal Semantics. I hope to know a bit more about Machine Learning as well as Formal Syntax in the near future.</p>
+      <h1>About</h1>
+      <p>Hello, my name is Guilherme Teixeira. I'm a software developer, and a linguistics undergraduate student based in São Carlos, Brazil.</p>
+      <p>I love computers and inline-skating since the childhood; and I love to read about linguistics and science in general. I love to read science fiction, and fantasy literature too. I also enjoy watching NBA.</p>
+      <p>I'm currently improving my JavaScript and React skills in my job as a front-end developer. In the college, my main goal is to develop research on Generative Grammar (aka Chomskyan Syntax) and Language Acquisition.</p>
     </S.Content>
 
 
---
+++ src/pages/about.styles.js
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled"
+
+export const Content = styled.div`
+  margin: 0 auto;
+  max-width: 860px;
+  padding: 1.45rem 1.0875rem;
+`
