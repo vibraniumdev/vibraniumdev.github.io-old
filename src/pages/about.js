b1cdadca75b0e68ae6c7d13c2b40414b4f3356ac
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2021-02-17 00:28:20 -0300

    Tries to fix linting in order to build to work

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,19 +2,28 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import * as S from './about.styles'
+import * as S from "./about.styles"
 
 const About = () => (
   <Layout>
     <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
-    <S.Content>
+    <S.AboutContent>
       <h1>About</h1>
-      <p>Hello, my name is Guilherme Teixeira. I'm a software developer, and a linguistics undergraduate student based in São Carlos, Brazil.</p>
-      <p>I love computers and inline-skating since the childhood; and I love to read about linguistics and science in general. I love to read science fiction, and fantasy literature too. I also enjoy watching NBA.</p>
-      <p>I'm currently improving my JavaScript and React skills in my job as a front-end developer. In the college, my main goal is to develop research on Generative Grammar (aka Chomskyan Syntax) and Language Acquisition.</p>
-    </S.Content>
-
-
+      <p>
+        Hello, my name is Guilherme Teixeira. I'm a software developer, and a
+        linguistics undergraduate student based in São Carlos, Brazil.
+      </p>
+      <p>
+        I love computers and inline-skating since the childhood; and I love to
+        read about linguistics and science in general. I love to read science
+        fiction, and fantasy literature too. I also enjoy watching NBA.
+      </p>
+      <p>
+        I'm currently improving my JavaScript and React skills in my job as a
+        front-end developer. In the college, my main goal is to develop research
+        on Generative Grammar (aka Chomskyan Syntax) and Language Acquisition.
+      </p>
+    </S.AboutContent>
   </Layout>
 )
 
--- src/pages/about.styles.js
+++ src/pages/about.styles.js
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled"
 
-export const Content = styled.div`
+export const AboutContent = styled.div`
   margin: 0 auto;
   max-width: 860px;
   padding: 1.45rem 1.0875rem;
