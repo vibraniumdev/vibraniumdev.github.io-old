8f5f7673987322d2e9adb3bba7602fd51148a893
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2021-04-04 00:14:25 -0300

    Updates

--- component---src-pages-404-js-13f8f1de6ab711a8e984.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_julia=self.webpackChunkgatsby_starter_julia||[]).push([[883],{9616:function(t,u,e){"use strict";e.r(u);e(7294);var s=e(2684),a=e(6179),n=e(5159);u.default=function(){return(0,n.tZ)(s.Z,null,(0,n.tZ)(a.Z,{title:"404: Not found"}),(0,n.tZ)("h1",null,"NOT FOUND"),(0,n.tZ)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-13f8f1de6ab711a8e984.js.map
