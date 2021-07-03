7f2237077abc96097162435a558723606fc274f1
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-07-03 13:32:28 -0300

    Quick fix: import styled components as "S"

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,25 +1,25 @@
 import React from "react"
-import { Wrapper, Content, Stats, Card } from "./projectCard.styles"
+import * as S from "./projectCard.styles"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCode, faStar } from "@fortawesome/free-solid-svg-icons"
 
 const ProjectCard = ({ reposList, isLoading }) => (
-  <Wrapper>
+  <S.Wrapper>
     {reposList &&
       !isLoading &&
       reposList.map(
         ({ name, description, language, html_url, stargazers_count }) => (
-          <Card
+          <S.Card
             aria-current
             rel="noopener noreferrer"
             target="_blank"
             href={html_url}
           >
-            <Content>
+            <S.Content>
               <h4>{name}</h4>
               <p>{description}</p>
-            </Content>
-            <Stats>
+            </S.Content>
+            <S.Stats>
               <div>
                 <FontAwesomeIcon className="icon" icon={faCode} size="1x" />
                 <span>{language}</span>
@@ -32,10 +32,10 @@ const ProjectCard = ({ reposList, isLoading }) => (
                 />
                 <span>{stargazers_count}</span>
               </div>
-            </Stats>
-          </Card>
+            </S.Stats>
+          </S.Card>
         )
       )}
-  </Wrapper>
+  </S.Wrapper>
 )
 export default ProjectCard
