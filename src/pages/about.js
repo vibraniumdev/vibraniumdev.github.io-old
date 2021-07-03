9a0fd4345ac416b1b90dc9755a542f289aea62e3
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-07-03 01:03:09 -0300

    Translates about page to english

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,37 +7,38 @@ import { AboutContent } from "../pagesStyles/about.styles"
 
 const About = () => (
   <Layout>
-    <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO
+      title="About"
+      keywords={[`javascript`, `programming`, `react`, `linguistics`]}
+    />
     <AboutContent>
       <h1>About me</h1>
       <p>
-        Meu nome é Guilherme Teixeira, desenvolvedor front-end e graduando em
-        Linguística.
+        Hello! My name is Guilherme Teixeira. I'm a front-end developer, and an
+        undergraduate Linguistics student.
       </p>
       <p>
-        Amo computadores, música, patins e basquete desde a infância; e adoro
-        ler sobre linguística e ciência. Também gosto de ler ficção científica e
-        ficção fantástica.
+        I love computers, progressive metal, rollerblades, and NBA since the
+        childhood. I also enjoy reading about science fiction, fantasy, and
+        philosophy. When I grew older, I've found out that I also love science and grammar, so I decided to join a bachelor degree called Linguistics, which is the one that has language as its main scientific object of study.
       </p>
       <p>
-        Desde a infância sou fascinado por computadores, música, patins,
-        snowboarding e basquete.
+        Although my tech career began almost 10 years ago, I've made my first
+        steps into software development in 2017 when I started learning Python
+        in the college. One year later, I was already working as a Python
+        software development intern. Later I took the oportunity to work as a
+        computational linguist (NLP) intern. In 2020, I accepted the challenge
+        of starting over.
       </p>
       <p>
-        Embora minha trajetória na tecnologia tenha se iniciado há quase 10
-        anos, meus primeiros passos no desenvolvimento de software foram dados
-        em 2017, quando comecei a aprender Python. Entre 2018 e 2019 trabalhei
-        com Python como estagiário, até que tive a oportunidade de ingressar
-        como estagiário em Linguística Computacional, para aliar meus
-        conhecimentos de Python e NLP. Em 2020, decidi mudar de carreira e ir
-        trabalhar com desenvolvimento Front-End. Atualmente, em meu trabalho
-        como desenvolvedor, estou aprimorando meus
-        conhecimentos sobre JavaScript, em especial em ReactJS, GatsbyJS e
-        NextJS, além de obviamente HTML e CSS e seus derivados.
+        Currently I work as a front-end developer at the biggest online printing
+        company in Brazil. I'm constantly improving my knowledge about HTML,
+        CSS, and JavaScript, as well as ReactJS, NextJS and GatsbyJS.
       </p>
       <p>
-        Na universidade, meu principal interesse é a linguística teórica, sobretudo nos tópicos relacionados a Epistemologia e
-        Filosofia Analítica, Sintaxe Gerativa, Semântica Formal e Aquisição de Linguagem.
+        As a college student, my topics of interest are theoretical linguistics,
+        mainly Generative Syntax, Formal Semantics, Epistemology, Philosophy of
+        Language (Analytic Philosophy), and Language Acquisition.
       </p>
     </AboutContent>
   </Layout>
