d9e7ebb81f0f6191bdb0af0d63894c83b671c5d3
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-10-09 12:16:05 -0300

    Removes unused GitHubLink style

--- src/components/header.styles.js
+++ src/components/header.styles.js
@@ -38,32 +38,6 @@ export const NavLink = styled(Link)`
   }
 `
 
-export const GitHubLink = styled.a`
-  color: black;
-  margin-left: 15px;
-  text-decoration: none;
-  display: inline-block;
-  position: relative;
-
-  ::after {
-    content: "";
-    position: absolute;
-    width: 100%;
-    transform: scaleX(0);
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.8);
-    transform-origin: bottom right;
-    transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
-  }
-
-  :hover::after {
-    transform: scaleX(1);
-    transform-origin: bottom left;
-  }
-`
-
 export const HomeLink = styled(NavLink)`
   margin-left: 0;
 
