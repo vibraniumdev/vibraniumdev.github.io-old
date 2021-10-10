7125da1e4858b12975a9b34bb6e1f9cba691b342
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-10-09 21:24:15 -0300

    NewHoverLink | Creates a new hover link behavior

--- src/components/header.styles.js
+++ src/components/header.styles.js
@@ -13,32 +13,40 @@ export const NavLink = styled(Link)`
   text-decoration: none;
   display: inline-block;
   position: relative;
+  transition: 0.4s ease-out;
 
-  ::after {
-    content: "";
-    position: absolute;
-    width: 100%;
-    transform: scaleX(0);
-    height: 0.125rem;
-    bottom: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.8);
-    transform-origin: bottom right;
-    transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
+  :nth-child(1) {
+    margin-left: 0;
   }
 
-  :hover::after {
-    transform: scaleX(1);
-    transform-origin: bottom left;
+  ::after {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 3px;
+    top: 100%;
+    left: 0;
+    background-color: #6c757d;
+    transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
+    transform: scaleX(0);
+    transform-origin: right;
   }
 
   &.active {
     color: #6c757d;
-    /* text-decoration: underline; */
+    :after {
+      transform: scaleX(1);
+    }
   }
 
-  :nth-child(1) {
-    margin-left: 0;
+  :hover {
+    color: #6c757d;
+    transition: 0.4s ease-in;
+
+    :after {
+      transform: scaleX(1);
+      transform-origin: left;
+    }
   }
 `
 
