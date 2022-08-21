62a75a2b51d1c2a8c10d9ad3164fb6aa85037a1d
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2022-08-21 16:02:42 -0300

    Puts lists inside content and exports it

---
+++ src/content/aboutLists.js
@@ -0,0 +1,168 @@
+export const gearList = [
+  {
+    category: "Primary Laptop",
+    name: "Dell Inspiron 7572 (16GB RAM, 128GB SSD + 1TB HDD)",
+    link: "",
+  },
+  {
+    category: "Secondary Laptop",
+    name: "MacBook Air early-2015, 8GB RAM, 128GB",
+    link: "https://support.apple.com/kb/SP714?viewlocale=pt_BR&locale=en_KW",
+  },
+  {
+    category: "Mouse",
+    name: "Logitech MX Anywhere 2S",
+    link: "https://www.logitech.com/en-us/eol/mx-anywhere-2s-mouse.910-005132.html",
+  },
+  {
+    category: "Keyboard",
+    name: "Keychron K2 (Hot-Swappable) Wireless Mechanical Keyboard (Version 2)",
+    link: "https://www.keychron.com/products/keychron-k2-hot-swappable-wireless-mechanical-keyboard",
+  },
+  {
+    category: "External Monitor",
+    name: "Dell SE2216H, 21.5, 1920x1080",
+    link: "",
+  },
+  {
+    category: "Headphones",
+    name: "Edifier W820BT",
+    link: "https://edifier.com.br/headphone-bluetooth-over-ear-edifier-w820bt.html",
+  },
+  {
+    category: "Earphones",
+    name: "KZ ZS10 PRO",
+    link: "https://kz-audio.com/kz-zs10-pro.html",
+  },
+  {
+    category: "Smartphone",
+    name: "iPhone SE (2nd generation)",
+    link: "https://support.apple.com/kb/SP820?locale=pt_BR",
+  },
+]
+
+export const softwareList = [
+  {
+    category: "Main Operating System",
+    name: "Linux Mint",
+    link: "https://linuxmint.com/",
+  },
+  {
+    category: "Secondary Operating System",
+    name: "Arch Linux ",
+    link: "https://archlinux.org/",
+  },
+  {
+    category: "Fallback Operating System",
+    name: "macOS Monterey",
+    link: "https://www.apple.com/macos/monterey/",
+  },
+  {
+    category: "Terminal",
+    name: "Kitty",
+    link: "https://sw.kovidgoyal.net/kitty/",
+  },
+  { category: "Shell", name: "Zsh", link: "https://www.zsh.org/" },
+  {
+    category: "Browser",
+    name: "Firefox Developer Edition",
+    link: "https://www.mozilla.org/en-US/firefox/developer/",
+    subcategory: [
+      { name: "Vimium", link: "https://github.com/philc/vimium" },
+      {
+        name: "uBlock Origin",
+        link: "https://github.com/gorhill/uBlock#ublock-origin",
+      },
+      {
+        name: "React Developer Tools",
+        link: "https://addons.mozilla.org/en-US/firefox/addon/react-devtools/",
+      },
+      {
+        name: "Auto Tab Discard",
+        link: "https://add0n.com/tab-discard.html",
+      },
+      { name: "Bitwarden", link: "https://bitwarden.com/download/" },
+      { name: "Clockify", link: "https://clockify.me/apps" },
+      {
+        name: "Multi-Account Containers",
+        link: "https://github.com/mozilla/multi-account-containers#readme",
+      },
+      {
+        name: "Tweak New Twitter",
+        link: "https://github.com/insin/tweak-new-twitter/",
+      },
+    ],
+  },
+  {
+    category: "Note-taking",
+    name: "Simplenote",
+    link: "https://simplenote.com/",
+  },
+  {
+    category: "Second Brain",
+    name: "Notion",
+    link: "https://www.notion.so/",
+  },
+  {
+    category: "Code Editor",
+    name: "Neovim (LunarVim)",
+    link: "https://www.lunarvim.org/",
+    subcategory: [
+      {
+        name: "Indent Blankline",
+        link: "https://github.com/lukas-reineke/indent-blankline.nvim",
+      },
+      {
+        name: "Onenord Theme",
+        link: "https://github.com/rmehri01/onenord.nvim",
+      },
+      {
+        name: "LunarVimColorschemes",
+        link: "https://github.com/LunarVim/Colorschemes",
+      },
+      {
+        name: "Everforest Theme",
+        link: "https://github.com/sainnhe/everforest",
+      },
+      {
+        name: "Dracula Theme",
+        link: "https://github.com/Mofiqul/dracula.nvim",
+      },
+      {
+        name: "Gruvbox Theme",
+        link: "https://github.com/ellisonleao/gruvbox.nvim",
+      },
+      {
+        name: "Persistence",
+        link: "https://github.com/folke/persistence.nvim",
+      },
+      { name: "Surround", link: "https://github.com/tpope/vim-surround" },
+    ],
+  },
+  {
+    category: "Main Font",
+    name: "JetBrains Mono",
+    link: "https://github.com/JetBrains/JetBrainsMono",
+  },
+  { category: "To-do Lists", name: "Todoist", link: "https://todoist.com/" },
+  {
+    category: "E-mail client",
+    name: "Mailspring",
+    link: "https://github.com/Foundry376/Mailspring",
+  },
+  {
+    category: "Time-Tracking",
+    name: "Clockify",
+    link: "https://clockify.me/",
+  },
+  {
+    category: "Pomodoro Timer",
+    name: "Pomodone",
+    link: "https://clockify.me/",
+  },
+  {
+    category: "Journalling",
+    name: "Journey",
+    link: "https://journey.cloud/",
+  },
+]
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,135 +4,10 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import TextCollapse from "../components/text-collapse"
 import BulletList from "../components/bullet-list"
+import { softwareList, gearList } from "../content/aboutLists"
 
 import * as S from "../pagesStyles/about.styles"
 const About = () => {
-  const softwareList = [
-    {
-      category: "Main Operating System",
-      name: "Linux Mint",
-      link: "https://linuxmint.com/",
-    },
-    {
-      category: "Secondary Operating System",
-      name: "Arch Linux ",
-      link: "https://archlinux.org/",
-    },
-    {
-      category: "Fallback Operating System",
-      name: "macOS Monterey",
-      link: "https://www.apple.com/macos/monterey/",
-    },
-    {
-      category: "Terminal",
-      name: "Kitty",
-      link: "https://sw.kovidgoyal.net/kitty/",
-    },
-    { category: "Shell", name: "Zsh", link: "https://www.zsh.org/" },
-    {
-      category: "Browser",
-      name: "Firefox Developer Edition",
-      link: "https://www.mozilla.org/en-US/firefox/developer/",
-      subcategory: [
-        { name: "Vimium", link: "https://github.com/philc/vimium" },
-        {
-          name: "uBlock Origin",
-          link: "https://github.com/gorhill/uBlock#ublock-origin",
-        },
-        {
-          name: "React Developer Tools",
-          link: "https://addons.mozilla.org/en-US/firefox/addon/react-devtools/",
-        },
-        {
-          name: "Auto Tab Discard",
-          link: "https://add0n.com/tab-discard.html",
-        },
-        { name: "Bitwarden", link: "https://bitwarden.com/download/" },
-        { name: "Clockify", link: "https://clockify.me/apps" },
-        {
-          name: "Multi-Account Containers",
-          link: "https://github.com/mozilla/multi-account-containers#readme",
-        },
-        {
-          name: "Tweak New Twitter",
-          link: "https://github.com/insin/tweak-new-twitter/",
-        },
-      ],
-    },
-    {
-      category: "Note-taking",
-      name: "Simplenote",
-      link: "https://simplenote.com/",
-    },
-    {
-      category: "Second Brain",
-      name: "Notion",
-      link: "https://www.notion.so/",
-    },
-    {
-      category: "Code Editor",
-      name: "Neovim (LunarVim)",
-      link: "https://www.lunarvim.org/",
-      subcategory: [
-        {
-          name: "Indent Blankline",
-          link: "https://github.com/lukas-reineke/indent-blankline.nvim",
-        },
-        {
-          name: "Onenord Theme",
-          link: "https://github.com/rmehri01/onenord.nvim",
-        },
-        {
-          name: "LunarVimColorschemes",
-          link: "https://github.com/LunarVim/Colorschemes",
-        },
-        {
-          name: "Everforest Theme",
-          link: "https://github.com/sainnhe/everforest",
-        },
-        {
-          name: "Dracula Theme",
-          link: "https://github.com/Mofiqul/dracula.nvim",
-        },
-        {
-          name: "Gruvbox Theme",
-          link: "https://github.com/ellisonleao/gruvbox.nvim",
-        },
-        {
-          name: "Persistence",
-          link: "https://github.com/folke/persistence.nvim",
-        },
-        { name: "Surround", link: "https://github.com/tpope/vim-surround" },
-      ],
-    },
-    {
-      category: "Main Font",
-      name: "JetBrains Mono",
-      link: "https://github.com/JetBrains/JetBrainsMono",
-    },
-    { category: "To-do Lists", name: "Todoist", link: "https://todoist.com/" },
-    {
-      category: "E-mail client",
-      name: "Mailspring",
-      link: "https://github.com/Foundry376/Mailspring",
-    },
-    {
-      category: "Time-Tracking",
-      name: "Clockify",
-      link: "https://clockify.me/",
-    },
-    {
-      category: "Pomodoro Timer",
-      name: "Pomodone",
-      link: "https://clockify.me/",
-    },
-    {
-      category: "Journalling",
-      name: "Journey",
-      link: "https://journey.cloud/",
-    },
-  ]
-
   return (
     <Layout>
       <Seo
@@ -142,7 +17,7 @@ const About = () => {
       <S.AboutContent>
         <h1>About Me</h1>
         <h2>Who I Am</h2>
-        <TextCollapse startsExpanded title="Short Bio">
+        <TextCollapse title="Short Bio">
           <p>
             {" "}
             I am Front-End Developer currently focused on building ReactJS and
@@ -388,82 +263,11 @@ const About = () => {
         <br />
         <br />
         <h2>What I Use</h2>
-        <TextCollapse title="Gear">
-          <ul>
-            <li>
-              {" "}
-              Primary Laptop: Dell Inspiron 7572, 16GB RAM, 128GB SSD + 1TB HDD{" "}
-            </li>
-            <li>
-              {" "}
-              Secondary Laptop:{" "}
-              <a
-                href="https://support.apple.com/kb/SP714?viewlocale=pt_BR&locale=en_KW"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                MacBook Air early-2015, 8GB RAM, 128GB{" "}
-              </a>{" "}
-            </li>
-            <li>
-              {" "}
-              Mouse:{" "}
-              <a
-                href="https://www.logitech.com/en-us/eol/mx-anywhere-2s-mouse.910-005132.html"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Logitech MX Anywhere 2S
-              </a>{" "}
-            </li>
-            <li>
-              Keyboard:{" "}
-              <a
-                href="https://www.keychron.com/products/keychron-k2-hot-swappable-wireless-mechanical-keyboard"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Keychron K2 (Hot-Swappable) Wireless Mechanical Keyboard
-                (Version 2)
-              </a>
-            </li>
-            <li> External monitor: Dell SE2216H, 21.5", 1920x1080 </li>
-            <li>
-              {" "}
-              Headphones:{" "}
-              <a
-                href="https://edifier.com.br/headphone-bluetooth-over-ear-edifier-w820bt.html"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Edifier W820BT
-              </a>
-            </li>
-            <li>
-              {" "}
-              Earphones:{" "}
-              <a
-                href="https://kz-audio.com/kz-zs10-pro.html"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                KZ ZS10 PRO
-              </a>
-            </li>
-            <li>
-              Smartphone:{" "}
-              <a
-                href="https://support.apple.com/kb/SP820?locale=pt_BR"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                iPhone SE (2nd generation)
-              </a>
-            </li>
-          </ul>
+        <TextCollapse startsExpanded title="Gear">
+          <BulletList topicList={gearList} />
         </TextCollapse>
 
-        <TextCollapse startsExpanded title="Software">
+        <TextCollapse title="Software">
           <BulletList topicList={softwareList} />
         </TextCollapse>
 
@@ -500,7 +304,7 @@ const About = () => {
         </TextCollapse>
         <br />
 
-        <TextCollapse startsExpanded title="Other Links">
+        <TextCollapse title="Other Links">
           <ul>
             <li>
               <a
