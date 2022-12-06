488524596a556227c45fcd607e0d7429a5110566
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2022-12-06 15:49:38 -0300

    Updates layout

--- src/pages/pages-counter.js
+++ src/pages/pages-counter.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react"
 
 import Layout from "../components/layout"
 import * as S from "../pagesStyles/pagesCounter.styles"
-import { Divider } from "@mui/material"
+import { Divider, Paper, Button, Card } from "@mui/material"
 
 const PagesCounter = () => {
   const [initialPage, setInitialPage] = useState(267) // deixar 0 como padrão
@@ -36,47 +36,66 @@ const PagesCounter = () => {
   return (
     <Layout>
       <S.Content>
-        <S.ContentRow>
-          <S.NumberInput
-            type="number"
-            label="Página inicial"
-            onChange={handleInitialPage}
-            helperText={`Inserido: ${initialPage}`}
-          />
-          <S.NumberInput
-            type="number"
-            label="Página final"
-            onChange={handleLastPage}
-            helperText={`Inserido: ${lastPage}`}
-          />
-        </S.ContentRow>
-        <S.ContentRow>
-          <p>Total a ser lido: {totalPagesToRead} páginas.</p>
-        </S.ContentRow>
+        <Card sx={{ marginBottom: "1rem" }} variant="outlined">
+          <S.ContentColumn>
+            <S.NumberInput
+              type="number"
+              label="Página inicial"
+              onChange={handleInitialPage}
+              helperText={`Inserido: ${initialPage}`}
+              sx={{ marginRight: "1rem" }}
+            />
+            <S.NumberInput
+              type="number"
+              label="Página final"
+              onChange={handleLastPage}
+              helperText={`Inserido: ${lastPage}`}
+            />
+          </S.ContentColumn>
+          <S.ContentRow>
+            <Paper elevation="3">
+              <S.PaperText paragraph variant="string">
+                Total a ser lido: {totalPagesToRead} páginas
+              </S.PaperText>
+            </Paper>
+          </S.ContentRow>
+        </Card>
 
-        <S.ContentRow>
-          <Divider orientation="horizontal" />
-        </S.ContentRow>
+        <Card variant="outlined">
+          <S.ContentColumn>
+            <S.NumberInput
+              sx={{ marginRight: "1rem" }}
+              // helperText={`Página atual: ${currentPage || 0}`}
+              label="Última página lida"
+              onChange={handleCurrentPage}
+            />
+            {/* <Button size="large" variant="contained"> */}
+            {/*   Calcular */}
+            {/* </Button> */}
+          </S.ContentColumn>
+          <S.ContentRow>
+            <Paper elevation="3">
+              <S.PaperText>Página atual: {currentPage || 0}</S.PaperText>
+            </Paper>
+          </S.ContentRow>
 
-        <S.ContentRow>
-          <S.NumberInput
-            helperText={`Página atual: ${currentPage || 0}`}
-            label="Última página lida"
-            onChange={handleCurrentPage}
-          />
-        </S.ContentRow>
+          <S.ContentRow>
+            <Paper sx={{ marginBottom: "1rem" }} elevation="3">
+              <S.PaperText>
+                Você já leu {currentPage - initialPage} das{" "}
+                {lastPage - initialPage} páginas (
+                {lastPage - initialPage - (currentPage - initialPage)}{" "}
+                restantes)
+              </S.PaperText>
+            </Paper>
 
-        <S.ContentRow>
-          <p>
-            Você já leu {currentPage - initialPage} das {lastPage - initialPage}{" "}
-            páginas
-          </p>
-          <p>
-            Faltam {lastPage - initialPage - (currentPage - initialPage)}{" "}
-            páginas
-          </p>
-          <p>Você já leu um total de {percentageRead.toFixed(2)}%</p>
-        </S.ContentRow>
+            <Paper sx={{ marginBottom: "1rem" }} elevation="3">
+              <S.PaperText>
+                Você já leu um total de {percentageRead.toFixed(2)}%
+              </S.PaperText>
+            </Paper>
+          </S.ContentRow>
+        </Card>
       </S.Content>
     </Layout>
   )
--- src/pagesStyles/pagesCounter.styles.js
+++ src/pagesStyles/pagesCounter.styles.js
@@ -1,10 +1,17 @@
 import styled from "@emotion/styled"
-import { TextField } from "@mui/material"
+import { TextField, Typography } from "@mui/material"
 
 export const Content = styled.div`
   margin: 0 auto;
   max-width: 860px;
   padding: 1.45rem 1.0875rem;
+`
+
+export const ContentColumn = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 1rem;
+  justify-content: space-between;
 `
 
 export const ContentRow = styled.div`
@@ -14,5 +21,9 @@ export const ContentRow = styled.div`
 `
 
 export const NumberInput = styled(TextField)`
-  margin-bottom: 2rem;
+  /* margin: 0 1rem 2rem 0; */
+`
+
+export const PaperText = styled(Typography)`
+  padding: 1rem;
 `
