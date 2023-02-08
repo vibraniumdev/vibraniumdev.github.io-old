8e5d58b0296f891c42fb76bcdf35f8d6a6348796
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2023-02-07 22:52:42 -0300

    Updates CNAME and adds the code related to the kinda-theme I added

---
+++ src/theme/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles"
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#64748B",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#0971f1",
+      darker: "#053e85",
+    },
+    neutral: {
+      main: "#6c757d",
+      contrastText: "#fff",
+    },
+  },
+})
