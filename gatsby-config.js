342984cd298959e8eb03a27c8dd2bfa86cfe88e0
ggteixeira <24235344+ggteixeira@users.noreply.github.com>
2023-10-03 18:59:55 -0300

    Update theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,8 +79,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        theme_color: `#6C757D`,
+        background_color: `#6C757D`,
         display: `minimal-ui`,
         icon: `src/images/profile-icon.png`, // This path is relative to the root of the site.
       },
