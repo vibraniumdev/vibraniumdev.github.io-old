c9cd8eacb057399ea1fd7344256ce79d13ddf7d6
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-12-08 17:31:15 -0300

    Updates

---
+++ component---src-pages-404-js-93807edd8ca8f4e537aa.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_julia=self.webpackChunkgatsby_starter_julia||[]).push([[125],{3773:function(t,s,u){u.r(s);u(758);var e=u(2847),a=u(2708),l=u(5530);s.default=()=>(0,l.jsx)(e.A,null,(0,l.jsx)(a.A,{title:"404: Not found"}),(0,l.jsx)("h1",null,"NOT FOUND"),(0,l.jsx)("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-93807edd8ca8f4e537aa.js.map
